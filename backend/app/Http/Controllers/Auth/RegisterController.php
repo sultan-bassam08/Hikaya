7cab4ff17dd8191c3f57d44b24a43d5ef769688c
hamzaszaitoun <hamzasaadzaitoun@gmail.com>
2024-12-06 15:03:00 +0300

    pull

--- backend/app/Http/Controllers/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Auth/RegisterController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-
+use Illuminate\Support\Facades\Log;
 class RegisterController extends Controller
 {
 public function register(Request $request)
