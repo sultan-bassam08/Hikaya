5f95000852d31f22f5a7b773f7f2699c74880358
OmarAbed16 <OmarFathiAbed@gmail.com>
2024-12-06 15:41:55 +0300

    upload omar tasks #2

--- backend/resources/views/ai/chatbot.blade.php
+++ backend/resources/views/ai/chatbot.blade.php
@@ -11,7 +11,6 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     
      <!-- Include jQuery from a CDN -->
- 
      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
      
