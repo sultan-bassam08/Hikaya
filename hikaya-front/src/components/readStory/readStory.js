56561f8d1860c411fb066cd3e1940252a778b356
EsraaEid2 <esraa.eidd2@gmail.com>
2024-12-09 09:46:05 +0300

    pull

--- hikaya-front/src/components/readStory/readStory.js
+++ hikaya-front/src/components/readStory/readStory.js
@@ -41,27 +41,132 @@ const StoryDetail = () => {
         <div className="row align-items-start">
           <div className="col-lg-8 m-15px-tb">
             <article className="article">
-              <div className="article-img">
-                <img
-                  style={{
-                    width: "100%",
-                    maxHeight: "500px",
-                    objectFit: "cover",
-                  }}
-                  src={
-                    story.story_picture ||
-                    "https://www.bootdey.com/image/800x350/87CEFA/000000"
-                  } // Default image if story picture is not available
-                  title={story.title}
-                  alt="Story"
-                />
-              </div>
+            <div className="article-img">
+            <img
+              style={{
+                width: "100%",            
+                maxWidth: "100%",         
+                maxHeight: "350px",    
+                height: "auto",         
+                objectFit: "contain",
+              }}
+              src={story.story_picture || "https://www.bootdey.com/image/800x350/87CEFA/000000"}  // Default image if none available
+              title={story.title}
+              alt="Story"
+            />
+          </div>
+
               <div className="article-title">
                 <h6>
                   <a href="#">{story.category.category_name} story</a>
                 </h6>
                 <h2>{story.title}</h2>
-                <div className="media">
+                
+              </div>
+              <div className="article-content">
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: story.content,
+                  }}
+                />
+              </div>
+            </article>
+          </div>
+          <div className="col-lg-4 m-15px-tb blog-aside">
+            {/* Author */}
+            <div className="widget widget-author">
+              <div className="widget-title">
+                <h3>Meet the Author!</h3>
+              </div>
+              <div className="media media-flexed">
+                  <div className="avatar">
+                    <img
+                      src={story.user.profile_picture}
+                      title="Author Avatar"
+                      alt="Author"
+                    />
+                  </div>
+                  <div className="media-body">
+                    <Link to={`/user-profile/${story.user.id}`}>
+                      {story.user.first_name + " " + story.user.last_name ||
+                      "Unknown Author"}
+                    </Link>
+                    <span>
+                      {new Date(story.updated_at).toLocaleDateString() ||
+                        "Unknown Date"}
+                    </span>
+                  </div>
+                </div>
+              
+            </div>
+            {/* End Author */}
+
+            {/* Latest Post */}
+            <div className="widget widget-latest-post">
+              <div className="widget-title">
+                <h3>More stories</h3>
+              </div>
+              <div className="widget-body">
+              {otherStories.length > 0 ? (
+                <ul>
+                  {otherStories.map((otherStory) => (
+                    <li key={otherStory.story_id} className="story-item">
+                      <div className="story-thumbnail">
+                        <img
+                          src={otherStory.story_picture} // The image of the story
+                          alt={otherStory.title}
+                          className="story-img"
+                        />
+                      </div>
+                      <div className="story-title">
+                        <Link to={`/readStory/${otherStory.story_id}`}>
+                          {otherStory.title}
+                        </Link>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>No other stories available.</p>
+              )}
+            </div>
+
+            </div>
+            {/* End Latest Post */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StoryDetail;
+/* <div className="widget-body">
+                <div className="media align-items-center media-flexed">
+                  <div className="avatar">
+                    <img
+                      src={story.user.profile_picture}
+                      title="Author Avatar"
+                      alt="Author"
+                    />
+                  </div>
+                  <div className="avatar-fixed">
+                    <img src={story.user.profile_picture} title="" alt="" />
+                  </div>
+                  <span className="media-body">
+                    <br />
+                    <h6>
+                      Hello, I'm{" "}
+                      {story.user.first_name + " " + story.user.last_name}
+                    </h6>
+                  </span>
+                </div>
+                <p>{story.user.bio}</p>
+              </div> */
+
+
+
+/* <div className="media">
                   <div className="avatar">
                     <img
                       src={story.user.profile_picture}
@@ -79,68 +184,4 @@ const StoryDetail = () => {
                         "Unknown Date"}
                     </span>
                   </div>
-                </div>
-              </div>
-              <div className="article-content">
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: story.content,
-                  }}
-                />
-              </div>
-            </article>
-          </div>
-          <div className="col-lg-4 m-15px-tb blog-aside">
-            {/* Author */}
-            <div className="widget widget-author">
-              <div className="widget-title">
-                <h3>Meet the Author!</h3>
-              </div>
-              <div className="widget-body">
-                <div className="media align-items-center">
-                  <div className="avatar">
-                    <img src={story.user.profile_picture} title="" alt="" />
-                  </div>
-                  <span className="media-body">
-                    <br />
-                    <h6>
-                      Hello, I'm{" "}
-                      {story.user.first_name + " " + story.user.last_name}
-                    </h6>
-                  </span>
-                </div>
-                <p>{story.user.bio}</p>
-              </div>
-            </div>
-            {/* End Author */}
-
-            {/* Latest Post */}
-            <div className="widget widget-latest-post">
-              <div className="widget-title">
-                <h3>More stories</h3>
-              </div>
-              <div className="widget-body">
-                {otherStories.length > 0 ? (
-                  <ul>
-                    {otherStories.map((otherStory) => (
-                      <li key={otherStory.story_id}>
-                        <Link to={`/readStory/${otherStory.story_id}`}>
-                          {otherStory.title}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                ) : (
-                  <p>No other stories available.</p>
-                )}
-              </div>
-            </div>
-            {/* End Latest Post */}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default StoryDetail;
+                </div> */
