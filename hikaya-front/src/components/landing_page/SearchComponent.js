a6be248a6078625c6105fdc6d8512bec635ecf92
OmarAbed16 <OmarFathiAbed@gmail.com>
2024-12-10 08:40:27 +0300

    pull funal version

--- hikaya-front/src/components/landing_page/SearchComponent.js
+++ hikaya-front/src/components/landing_page/SearchComponent.js
@@ -6,60 +6,48 @@ const SearchComponent = () => {
   const [query, setQuery] = useState("");
   const [selectedCategories, setSelectedCategories] = useState([]);
 
-  const categories = [
-    { name: "Adventure", icon: "fas fa-hiking" },
-    { name: "Drama", icon: "fas fa-theater-masks" },
-    { name: "Fantasy", icon: "fas fa-dragon" },
-    { name: "Horror", icon: "fas fa-ghost" },
-    { name: "Romance", icon: "fas fa-heart" },
-    { name: "Sci-Fi", icon: "fas fa-rocket" },
-  ];
-
   const handleInputChange = (event) => {
     setQuery(event.target.value);
   };
 
-  const toggleCategory = (index) => {
-    const categoryName = categories[index].name;
-    setSelectedCategories((prevCategories) =>
-      prevCategories.includes(categoryName)
-        ? prevCategories.filter((category) => category !== categoryName)
-        : [...prevCategories, categoryName]
+  const handleCategoryChange = (event) => {
+    const { value, checked } = event.target;
+    const categoryIndex = parseInt(value); // Convert the string value to an integer
+
+    setSelectedCategories(
+      (prevCategories) =>
+        checked
+          ? [...prevCategories, categoryIndex] // Add category index
+          : prevCategories.filter((category) => category !== categoryIndex) // Remove category index
     );
   };
 
   return (
     <>
       <div className="search-component">
-        <div className="search-bar">
-          <div className="search-input-container">
-            <i className="fas fa-search search-icon"></i>
-            <input
-              type="text"
-              placeholder="Search..."
-              value={query}
-              onChange={handleInputChange}
-              className="search-input"
-            />
-          </div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={query}
+          onChange={handleInputChange}
+          className="search-input"
+        />
+        <div className="checkbox-group">
+          {["Adventure", "Drama", "Fantasy", "Horror", "Romance", "Sci-Fi"].map(
+            (category, index) => (
+              <div key={index} className="checkbox-item">
+                <input
+                  type="checkbox"
+                  value={index + 1} // Store values starting from 1, not 0
+                  onChange={handleCategoryChange}
+                  className="category-checkbox"
+                />
+                <label>{category}</label>
+              </div>
+            )
+          )}
         </div>
-
-        <div className="categories-container">
-          <div className="categories">
-            {categories.map((category, index) => (
-              <button
-                key={index}
-                className={`category-button ${
-                  selectedCategories.includes(category.name) ? "active" : ""
-                }`}
-                onClick={() => toggleCategory(index)}
-              >
-                <i className={`${category.icon} category-icon`}></i>
-                {category.name}
-              </button>
-            ))}
-          </div>
-        </div>
+        <button className="search-button">Search</button>
       </div>
 
       {/* Pass query and selectedCategories as props to StoriesList */}
--- hikaya-front/src/components/story_components/storiesList.js
+++ hikaya-front/src/components/story_components/storiesList.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import './storiesList.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./storiesList.css";
 
 const StoriesList = ({ query, selectedCategories }) => {
   const [stories, setStories] = useState([]);
@@ -65,56 +65,71 @@ const StoriesList = ({ query, selectedCategories }) => {
           {filteredStories.map((story) => (
             <div className="col-lg-12 mb-4" key={story.story_id}>
               <article className="postcard dark blue">
-                <Link className="postcard__img_link" to={`/readStory/${story.story_id}`}>
+                <Link
+                  className="postcard__img_link"
+                  to={`/readStory/${story.story_id}`}
+                >
                   <img
                     className="postcard__img"
-                    src={story.story_picture || "https://via.placeholder.com/600x300"}
+                    src={
+                      story.story_picture ||
+                      "https://via.placeholder.com/600x300"
+                    }
                     alt={story.title}
                   />
                 </Link>
                 <div className="postcard__text">
                   <h1 className="postcard__title blue">
-                    <Link to={`/readStory/${story.story_id}`}>{story.title}</Link>
+                    <Link to={`/readStory/${story.story_id}`}>
+                      {story.title}
+                    </Link>
                   </h1>
-                 
-                  <div className="postcard__subtitle small">
-                  <span dateTime={story.created_at}>
-                    <i className="fas fa-calendar-alt mr-2"></i>
-                  {story.created_at ? new Date(story.created_at).toLocaleDateString() : 'N/A'}
-                  </span>
 
+                  <div className="postcard__subtitle small">
+                    <span dateTime={story.created_at}>
+                      <i className="fas fa-calendar-alt mr-2"></i>
+                      {story.created_at
+                        ? new Date(story.created_at).toLocaleDateString()
+                        : "N/A"}
+                    </span>
                   </div>
                   <div className="postcard__bar"></div>
                   <div
                     className="postcard__preview-txt"
                     dangerouslySetInnerHTML={{
-                      __html: story.content.substring(0, 100).replace(/\s+/g, ' ').trim() + "...",
+                      __html:
+                        story.content
+                          .substring(0, 100)
+                          .replace(/\s+/g, " ")
+                          .trim() + "...",
                     }}
                   ></div>
                   <ul className="postcard__tagbox">
                     <li className="tag__item">
-                      <i className="bi bi-tag"></i> {story.category.category_name}
+                      <i className="bi bi-tag"></i>{" "}
+                      {story.category.category_name}
                     </li>
-                    
+
                     <li className="tag__item play blue">
                       <Link to={`/readStory/${story.story_id}`}>
                         <i className="bi bi-book"></i> Read story now
                       </Link>
                     </li>
                   </ul>
-                   <span>
-        
-                      <button
-                        onClick={() => toggleLike(story.story_id)}
-                        className="btn btn-outline-danger btn-sm ml-2"
-                      >
-                        <i className={story.liked ? "fas fa-heart" : "far fa-heart"}></i>{" "}
-                        {story.liked ? "bookmarked" : "add to bookmark"}
-                      </button>
-                    
+                  <span>
+                    <button
+                      onClick={() => toggleLike(story.story_id)}
+                      className="btn btn-outline-danger btn-sm ml-2"
+                    >
+                      <i
+                        className={
+                          story.liked ? "fas fa-heart" : "far fa-heart"
+                        }
+                      ></i>{" "}
+                      {story.liked ? "bookmarked" : "add to bookmark"}
+                    </button>
                   </span>
                 </div>
-                
               </article>
             </div>
           ))}
